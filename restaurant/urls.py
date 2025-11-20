547843eddc47d2d74e98cb44eb028c6578022850
Darko Beovic <darkobeovic@Darkos-MacBook-Air.local>
2025-11-20 15:04:44 -0500

    Implements Menu API with DRF

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Página principal
     path('', views.index, name='index'),
-    # Dynamic routes
+
+    # Rutas dinámicas antiguas (las dejamos)
     path('menu/<str:menu_item>/', views.display_menu_item, name='display-menu'),
     path('menu/id/<int:item_id>/',
          views.display_menu_item_by_id, name='display-menu-id'),
+
+    # --- API Menu ---
+    path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(),
+         name='single-menu-item'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,10 @@
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
+from rest_framework import generics
 from django.http import HttpResponse
 from django.shortcuts import render
+
+# --- Vistas HTML para rutas estáticas ---
 
 
 def index(request):
@@ -12,3 +17,22 @@ def display_menu_item(request, menu_item):
 
 def display_menu_item_by_id(request, item_id):
     return HttpResponse(f"<h2>Menu item ID: {item_id}</h2>")
+
+
+# --- Vistas del API REST Framework ---
+
+# GET (lista), POST (crear)
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# GET (single), PUT, DELETE
+
+
+class SingleMenuItemView(
+    generics.RetrieveUpdateAPIView,
+    generics.DestroyAPIView
+):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
